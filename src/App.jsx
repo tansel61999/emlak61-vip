5ce5890bd1e24a6aa61136593f4bbb6bc21ab385
tansel61999 <tansel6199@gmail.com>
2026-02-06 20:51:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import Giris from './sayfalar/Giris';
+import Giris from './Sayfalar/Giris.jsx';
 import AnaPanel from './AnaPanel';
 
 const App = () => {
